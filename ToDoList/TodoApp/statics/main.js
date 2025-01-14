a8b27a8d0ce63c339937d09c2853692bebedf9ca
asif7649 <asifahma7761@gmail.com>
2025-01-15 00:36:33 +0530

    Add a TodoApp

---
+++ ToDoList/TodoApp/statics/main.js
@@ -0,0 +1,30 @@
+
+let add = document.querySelector('#add');
+let box = document.querySelector('.box');
+
+
+
+add.addEventListener('keyup',(e)=>{
+    if (e.key =='Enter'){
+if(add.value != ""){
+    add_todo(this.add.value)
+    add.value=""
+}
+    }
+})
+
+function add_todo(add_vl){
+    let div = document.createElement('div')
+        div.classList.add('list')
+    box.appendChild(div)
+    div.innerHTML=`<p>${add_vl}</p>
+    <li class='close'>x</li>`
+
+}
+box.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('close')){
+        console.log(e.target.classList)
+        e.target.parentNode.remove()
+    }
+})
+
