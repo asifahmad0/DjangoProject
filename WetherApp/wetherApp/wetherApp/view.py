a00240f619eae661930ac6c9eb4c322947e7adf1
asif7649 <asifahma7761@gmail.com>
2025-01-20 21:48:05 +0530

    fdsa

--- WetherApp/wetherApp/wetherApp/view.py
+++ WetherApp/wetherApp/wetherApp/view.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+import requests
+import datetime
 
 
 def home(request):
-    return HttpResponse(request, '<h1>My name is Asif Ahmad</h1>')
+   
+    if 'city' in request.POST:
+         city = request.POST['city']
+    else:
+         city = 'pakur'     
+    
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=d1684d03f3f1220d7e466dae6e412588'
+    PARAMS = {'units':'metric'}
+
+    data =requests.get(url,PARAMS).json()
+
+    description = data['weather'][0]['description']
+    icon = data['weather'][0]['icon']
+    temp = data['main']['temp']
+    day = datetime.date.today()
+    return render(request, 'index.html',{'city':city, 'description':description, 'icon':icon, 'temp': temp, 'day':day})
